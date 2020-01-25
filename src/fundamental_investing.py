32a5fa94ae65ea5505a1e469c85ca93d2b2a37a3
B. Bueno <bbueno25@hotmail.com>
2020-01-25 05:56:54 -0700

    edited fundamental_investing.py (Using Programming for Fundamental Investing Part 3)

--- src/fundamental_investing.py
+++ src/fundamental_investing.py
@@ -1,1 +1,35 @@
 
+import time
+import urllib
+
+sp500short = [
+    'a', 
+    'aa', 
+    'aapl', 
+    'abbv', 
+    'abc', 
+    'abt', 
+    'ace', 
+    'aci', 
+    'acn', 
+    'act', 
+    'adbe', 
+    'adi', 
+    'adm', 
+    'adp'
+    ]
+
+def key_statistics(stock):
+    """
+    Definitions:
+        pbr - price to book ratio
+    """
+    try:
+        source_code = urllib.urlopen('http://finance.yahoo.com' + stock).read()
+        pbr = source_code.split('Price/Book')[1].split('</td>')[0]
+        print('Price/Book:', pbr)
+    except Exception as e:
+        print('error:main loop:', str(e))
+
+if __name__ == '__main__':
+    key_statistics('aapl')
